bc0c6c549e97deab3009aeeffdea8f9f88afdb5b
Natalia Dorozynska <natala.dorozynska@gmail.com>
2019-02-21 22:28:16 +0100

    CrossSectionCharacteristics UTs.

--- WpfApp1.0.UnitTests/UnitTest1.cs
+++ WpfApp1.0.UnitTests/UnitTest1.cs
@@ -17,7 +17,22 @@ namespace WpfApp1._0.UnitTests
             characteristic.FiAs1 = 12;
             characteristic.Calculate();
             var result = Math.Round(characteristic.AreaAs1, 2);
-            var assert = 4.52;
+
+            // Assert
+            Assert.AreEqual(4.52, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arrange
+            var characteristic = new CrossSectionCharacteristics();
+
+            // Act
+            characteristic.CountAp = 4;
+            characteristic.FiAp = 12;
+            characteristic.Calculate();
+            var result = Math.Round(characteristic.AreaAp, 2);
 
             // Assert
             Assert.AreEqual(4.52, result);
