56d7415cfa334453604d74c052d30952a93a9206
Natalia Dorozynska <natala.dorozynska@gmail.com>
2019-03-16 19:20:40 +0100

    Enum with description ConcreteClass

--- WpfApp1.0/ConcreteParameters.cs
+++ WpfApp1.0/ConcreteParameters.cs
@@ -10,10 +10,13 @@ namespace WpfApp1._0
     class ConcreteParameters : INotifyPropertyChanged
     {
         private ConcreteClasses concreteClass = ConcreteClasses.C20;
-        private Double gamaF = 1.4;
-        // fck i fcd in [MPa]
+        private String concreteClassDescription = EnumDescribe.GetDescribe(ConcreteClasses.C20);
+        private Double gamaC = 1.5;
+        // fck and fcd in [MPa]
         private int fck = 20;
         private Double fcd = 0.0;
+        // Ecm in [Gpa]
+        private Double eCm = 30.0;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -22,7 +25,9 @@ namespace WpfApp1._0
             get
             { return concreteClass; }
             set
-            { concreteClass = value;
+            {
+                concreteClass = EnumDescribe.GetValueFromDescription<ConcreteClasses>(value.ToString());
+                
                 UpdateData();
             }
         }
@@ -31,7 +36,48 @@ namespace WpfApp1._0
         {
             get
             {
-                return Enum.GetValues(typeof(ConcreteClasses));
+                var abc =  Enum.GetValues(typeof(ConcreteClasses));
+                List<ConcreteClasses> arr = abc.OfType<ConcreteClasses>().ToList();
+                List<String> result = new List<string>();
+                foreach (ConcreteClasses cC in arr)
+                {
+                    var descript = EnumDescribe.GetDescribe(cC);
+                    result.Add(descript);
+                }
+                return result.ToArray();
+            }
+        }
+
+
+        public String ConcreteClassDescription
+        {
+            set
+            {
+                concreteClassDescription = value;
+                concreteClass = EnumDescribe.GetValueFromDescription<ConcreteClasses>(concreteClassDescription);
+                UpdateData();
+                switch (concreteClass)
+                {
+                    case ConcreteClasses.C16:
+                        fck = 16;
+                        eCm = 29;
+                        Calculate();
+                        break;
+                    case ConcreteClasses.C20:
+                        fck = 20;
+                        eCm = 30;
+                        Calculate();
+                        break;
+                    case ConcreteClasses.C25:
+                        fck = 25;
+                        eCm = 31;
+                        Calculate();
+                        break;
+                }
+            }
+            get
+            {
+                return concreteClassDescription;
             }
         }
 
@@ -39,17 +85,23 @@ namespace WpfApp1._0
         {
             Calculate();
             PropertyChanged(this, new PropertyChangedEventArgs("ConcreteClass"));
+            PropertyChanged(this, new PropertyChangedEventArgs("ConcreteClassDescription"));
         }
 
         public void Calculate()
         {
-            fcd = fck / gamaF;
+            concreteClassDescription = EnumDescribe.GetDescribe(concreteClass);
+            fcd = fck / gamaC;
         }
 
         public enum ConcreteClasses
-        { C16,
-          C20,
-          C25
+        {
+            [Description("C16/20")]
+            C16,
+            [Description("C20/25")]
+            C20,
+            [Description("C25/30")]
+            C25
         }
     }
 }
---
+++ WpfApp1.0/EnumDescribe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1._0
+{
+    public static class EnumDescribe
+    {
+        public static String GetDescribe(Enum e)
+        {
+            var attribute = e.GetType()
+                .GetTypeInfo()
+                .GetMember(e.ToString())
+                .FirstOrDefault(member => member.MemberType == MemberTypes.Field)
+                .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .SingleOrDefault()
+                as DescriptionAttribute;
+            return attribute?.Description ?? e.ToString(); ;
+        }
+
+        public static T GetValueFromDescription<T>(String description)
+        {
+            var type = typeof(T);
+            if (!type.IsEnum)
+                throw new ArgumentException();
+            FieldInfo[] fields = type.GetFields();
+            var field = fields
+                            .SelectMany(f => f.GetCustomAttributes(
+                                typeof(DescriptionAttribute), false), (
+                                    f, a) => new { Field = f, Att = a })
+                            .Where(a => ((DescriptionAttribute)a.Att)
+                                .Description == description).SingleOrDefault();
+            return field == null ? default(T) : (T)field.Field.GetRawConstantValue();
+        }
+    }
+}
