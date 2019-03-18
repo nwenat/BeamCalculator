00cdde3bfdcadb31d2df30429e4d72fd2b7a6bcc
Natalia Dorozynska <natala.dorozynska@gmail.com>
2019-03-18 13:56:57 +0100

    concrete parameters seted

--- WpfApp1.0/ConcreteParameters.cs
+++ WpfApp1.0/ConcreteParameters.cs
@@ -16,12 +16,98 @@ namespace WpfApp1._0
         private int gamaB = 24;
         // fck and fcd in [MPa]
         private int fck = 40;
-        private Double fcd = 0.0;
+        private Double fcm = 48.0;
+        private Double fcd = 26.67;
+        private Double fctk = 2.5;
+        private Double fctm = 3.5;
+        private Double fctd = 1.67;
         // Ecm in [Gpa]
         private Double eCm = 35.0;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public Double GamaC
+        {
+            get
+            {
+                return gamaC;
+            }
+        }
+
+        public int GamaB
+        {
+            get
+            {
+                return gamaB;
+            }
+            set
+            {
+                gamaB = value;
+            }
+        }
+        public int Fck
+        {
+            get
+            {
+                return fck;
+            }
+        }
+
+        public Double Fcm
+        {
+            get
+            {
+                return fcm;
+            }
+        }
+
+        public Double Fcd
+        {
+            get
+            {
+                return fcd;
+            }
+        }
+
+        public Double Fctk
+        {
+            get
+            {
+                return fctk;
+            }
+        }
+
+        public Double Fctm
+        {
+            get
+            {
+                return fctm;
+            }
+        }
+
+        public Double Fctd
+        {
+            get
+            {
+                return fctd;
+            }
+        }
+
+        public Double ECm
+        {
+            get
+            {
+                return eCm;
+            }
+        }
+
+        public Array GammaBArray
+        {
+            get
+            {
+                return new int[] { 24, 25 };
+            }
+        }
         public Array TypesOfConcrete
         {
             get
@@ -47,14 +133,23 @@ namespace WpfApp1._0
                 {
                     case ConcreteClasses.C40:
                         fck = 40;
+                        fcm = 48;
+                        fctk = 2.5;
+                        fctm = 3.5;
                         eCm = 35;
                         break;
                     case ConcreteClasses.C45:
                         fck = 45;
+                        fcm = 53;
+                        fctk = 2.7;
+                        fctm = 3.8;
                         eCm = 36;
                         break;
                     case ConcreteClasses.C50:
                         fck = 50;
+                        fcm = 58;
+                        fctk = 2.9;
+                        fctm = 4.1;
                         eCm = 37;
                         break;
                 }
@@ -71,12 +166,22 @@ namespace WpfApp1._0
             Calculate();
             PropertyChanged(this, new PropertyChangedEventArgs("ConcreteClass"));
             PropertyChanged(this, new PropertyChangedEventArgs("ConcreteClassDescription"));
+            PropertyChanged(this, new PropertyChangedEventArgs("GamaC"));
+            PropertyChanged(this, new PropertyChangedEventArgs("Fck"));
+            PropertyChanged(this, new PropertyChangedEventArgs("Fcm"));
+            PropertyChanged(this, new PropertyChangedEventArgs("Fcd"));
+            PropertyChanged(this, new PropertyChangedEventArgs("Fctk"));
+            PropertyChanged(this, new PropertyChangedEventArgs("Fctm"));
+            PropertyChanged(this, new PropertyChangedEventArgs("Fctd"));
+            PropertyChanged(this, new PropertyChangedEventArgs("ECm"));
+            PropertyChanged(this, new PropertyChangedEventArgs("GamaB"));
         }
 
         public void Calculate()
         {
             concreteClassDescription = EnumDescribe.GetDescribe(concreteClass);
             fcd = fck / gamaC;
+            fctd = fctk / gamaC;
         }
 
         public enum ConcreteClasses
